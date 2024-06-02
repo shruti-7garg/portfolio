309a0cfdc829512e13d93eee8771536cda943e5a
shruti-7garg <sg456560@gmail.com>
2024-06-02 21:41:05 +0530

    files added

---
+++ index.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role",{
+    strings:[
+        "Front-end Developer",
+        "Web Developer",
+        "UI/UX Designer",
+        "Coder",
+    ],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:1000,
+    
+});
